adbfd8f732f4be56be754bf1129101a7aaf36123
Emiliano F Rodriguez <emilianofederico.mmo@outlook.com>
2022-07-26 19:14:03 -0300

    Creando el formulario con php y cambiando la imagen del header por un url

---
+++ formulario.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = $_POST ['name'];
+$email = $_POST ['email'];
+$title = $_POST ['title'];
+$tags = $_POST ['tags'];
+$description = $_POST ['description'];
+
+$mensaje = "Nombre del DJ" . $name . "\r\n";
+$mensaje = "Email de contacto" . $email . "\r\n";
+$mensaje = "Titulo del trac/set" . $title . "\r\n";
+$mensaje = "Hashtags" . $tags . "\r\n";
+$mensaje = "Descripcion" . $description . "\r\n";
+$mensaje = "Enviado el " . date('d/m/y', time());
+
+$destinatario = 'grooveenergy@gmail.com';
+$asunto = 'Este mail fue enviado desde Groove Energy WEB';
+
+mail($destinatario, $asunto, utf8_decode($mensaje4), $header);
+header('location:msjForm.html')
+?> 
