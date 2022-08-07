2418de29248284c385157d7e0753d4f65bbec8b4
Emiliano F Rodriguez <emilianofederico.mmo@outlook.com>
2022-08-07 20:17:46 -0300

    correcciones del formulario y creando el html exito

--- formulario.php
+++ formulario.php
@@ -1,21 +1,20 @@
 <?php
-
 $name = $_POST ['name'];
 $email = $_POST ['email'];
 $title = $_POST ['title'];
 $tags = $_POST ['tags'];
 $description = $_POST ['description'];
 
-$mensaje = "Nombre del DJ" . $name . "\r\n";
+$mensaje = "Nombre del DJ" . $name . ",\r\n";
 $mensaje = "Email de contacto" . $email . "\r\n";
 $mensaje = "Titulo del trac/set" . $title . "\r\n";
 $mensaje = "Hashtags" . $tags . "\r\n";
 $mensaje = "Descripcion" . $description . "\r\n";
 $mensaje = "Enviado el " . date('d/m/y', time());
 
-$destinatario = 'grooveenergy@gmail.com';
+$destinatario = 'emeleano.er@gmail.com';
 $asunto = 'Este mail fue enviado desde Groove Energy WEB';
 
-mail($destinatario, $asunto, utf8_decode($mensaje4), $header);
-header('location:msjForm.html')
+mail($destinatario, $asunto, utf8_decode($mensaje), $header);
+header('<location:exito.html');
 ?> 
